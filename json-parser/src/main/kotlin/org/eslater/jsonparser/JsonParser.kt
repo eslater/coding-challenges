154318d78d694389626d50e6de8f5c3137027317
Eric Slater <eslaterx@gmail.com>
2026-04-29 18:16:23 -0500

    basic single field object parsing is passing. failing test for object with multiple fields

--- json-parser/src/main/kotlin/org/eslater/jsonparser/JsonParser.kt
+++ json-parser/src/main/kotlin/org/eslater/jsonparser/JsonParser.kt
@@ -1,30 +1,82 @@
 package org.eslater.jsonparser
 
 class JsonParser {
-    fun parse(json: String) {
+    fun parse(json: String): JsonObject {
         val tokens: List<Token> = tokenize(json)
+        val tokenItr = tokens.iterator()
+        getNextTokenAndFailIfNotType(tokenItr, TokenType.LEFT_BRACKET)
+        val parsed = JsonObject()
+        val keyValueMap: MutableMap<String, JsonValue> = mutableMapOf()
+        while (tokenItr.hasNext()) {
+            val token = tokenItr.next()
+            if (token.type == TokenType.RIGHT_BRACKET) {
+                if (tokenItr.hasNext())
+                    throw JsonParseException("unexpected token ${token.type}")
+                parsed.value = keyValueMap;
+                break;
+            } else if (token.type == TokenType.DOUBLE_QUOTE) {
+                val key = getNextTokenAndFailIfNotType(tokenItr, TokenType.STRING)
+                getNextTokenAndFailIfNotType(tokenItr, TokenType.DOUBLE_QUOTE)
+                getNextTokenAndFailIfNotType(tokenItr, TokenType.COLON)
+                getNextTokenAndFailIfNotType(tokenItr, TokenType.DOUBLE_QUOTE)
+                val value = getNextTokenAndFailIfNotType(tokenItr, TokenType.STRING)
+                getNextTokenAndFailIfNotType(tokenItr, TokenType.DOUBLE_QUOTE)
+                keyValueMap.put(key.value, JsonString(value.value))
+            } else {
+                throw JsonParseException("unexpected next token ${token.type}")
+            }
+        }
+        return parsed
+    }
+
+    private fun getNextTokenAndFailIfNotType(iterator: Iterator<Token>, type: TokenType): Token {
+        if (!iterator.hasNext()) {
+            throw JsonParseException("Unexcepted end of input")
+        }
+        val token = iterator.next();
+        if (token.type != type) {
+            throw JsonParseException("unexpected next token, expected $type")
+        }
+        return token
     }
 
     fun tokenize(text: String): List<Token> {
         val tokens = mutableListOf<Token>()
-
+        var consumingWord = false
         val word = StringBuilder()
-        for (char in text) {
-            val token = when (char) {
-                '{' -> Token(TokenType.LEFT_BRACKET, char.toString())
-                '}' -> Token(TokenType.RIGHT_BRACKET, char.toString())
-                ':' -> Token(TokenType.COLON, char.toString())
-                '"' -> Token(TokenType.DOUBLE_QUOTE, char.toString())
-                else -> null
+        for (i in 0..<text.length) {
+            val char = text[i]
+            val token: Token? = when (char) {
+                '{' -> {
+                    Token(TokenType.LEFT_BRACKET, char.toString())
+                }
+                '}' -> {
+                    Token(TokenType.RIGHT_BRACKET, char.toString())
+                }
+                ':' ->  {
+                    Token(TokenType.COLON, char.toString())
+                }
+                '"' ->  {
+                    consumingWord = !consumingWord
+                    if (!consumingWord) {
+                        tokens.add(Token(TokenType.STRING, word.toString()))
+                        word.clear()
+                    }
+                    Token(TokenType.DOUBLE_QUOTE, char.toString())
+                }
+                else -> {
+                    if (consumingWord) {
+                        word.append(char)
+                    } else if (char.isWhitespace()) {
+                        continue
+                    } else {
+                        throw JsonParseException("unexpected character at position $i")
+                    }
+                    null
+                }
             }
             if (token != null) {
-                if (!word.isEmpty()) {
-                    tokens.add(Token(TokenType.STRING, word.toString()))
-                    word.clear()
-                }
                 tokens.add(token)
-            } else {
-                word.append(char)
             }
         }
         return tokens
@@ -38,5 +90,6 @@ enum class TokenType {
 data class Token(val type: TokenType, val value: String)
 
 sealed class JsonValue
-data class JsonObject(val value: Map<String, JsonValue>)
+data class JsonObject(var value: MutableMap<String, JsonValue> = mutableMapOf())
 data class JsonString(val value: String) : JsonValue()
+class JsonParseException(message: String) : Exception(message)
--- json-parser/src/test/kotlin/org/eslater/jsonparser/JsonParserTest.kt
+++ json-parser/src/test/kotlin/org/eslater/jsonparser/JsonParserTest.kt
@@ -1,13 +1,21 @@
 package org.eslater.jsonparser
 
-import kotlin.test.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ValueSource
 import kotlin.test.assertEquals
 
 class JsonParserTest {
 
-    @Test
-    fun `should tokenize the input`() {
-        var json = "{\"key\":\"value\"}"
+    @ParameterizedTest
+    @ValueSource(strings = [
+        "{\"key\":\"value\"}",
+        "{   \"key\":\"value\"    }",
+        "   {\"key\":\"value\"}  ",
+    ])
+    fun `should tokenize the input`(json: String) {
         var tokens: List<Token> = JsonParser().tokenize(json)
         var expected: List<Token> = listOf(
             Token(TokenType.LEFT_BRACKET, "{"),
@@ -19,7 +27,46 @@ class JsonParserTest {
             Token(TokenType.STRING, "value"),
             Token(TokenType.DOUBLE_QUOTE, "\""),
             Token(TokenType.RIGHT_BRACKET, "}"))
-
             assertEquals(expected, tokens)
     }
+
+    @ParameterizedTest
+    @MethodSource("jsonDocs")
+    fun `should parse and serialize the json document`(json: String, expected: JsonObject) {
+        val parser = JsonParser()
+        val result: JsonObject = parser.parse(json)
+        assertEquals(result, expected)
+    }
+
+    companion object {
+        @JvmStatic
+        fun jsonDocs() = listOf(
+            Arguments.of("{\"key\":\"value\"}",
+                JsonObject(mutableMapOf("key" to JsonString("value")))),
+            Arguments.of( "{\"key\":\"\"}",
+                JsonObject(mutableMapOf("key" to JsonString("")))),
+            Arguments.of( "{\"key\":\"value\",\"foo\":\"bar\"}",
+                JsonObject(mutableMapOf("key" to JsonString("value"), "foo" to JsonString("bar"))))
+        )
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = [
+        "xxx{\"key\":\"value\"}",
+        "{\"key\":\"value\"}xxx",
+        "{\"key\":\"value\"asdasd}",
+        "{asdasd\"key\":\"value\"asdasd}",
+        "{asdasd\"key\"::\"value\"asdasd}",
+        "{asdasd\"key\"\"\"::\"value\"asdasd}",
+        "{{\"key\"::\"value\"}",
+        "{\"key\"::\"value\"}}",
+        "{{\"key\"::\"value\"}}",
+    ])
+    fun `should throw JsonParse exception when the input is not valid json`(json: String) {
+        val parser = JsonParser()
+        assertThrows<JsonParseException> {
+            parser.parse(json)
+        }
+    }
+
 }
